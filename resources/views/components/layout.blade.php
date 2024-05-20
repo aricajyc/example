5a954591b91ed5a6f9aa9148c8764a5406c684cc
aricajyc <arica.joyce@s.unikl.edu.my>
2024-05-20 10:22:40 +0800

    Declare routes, and created views. Created first simple three page layout. Created layout file and learned it's functionality and how to use it.

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minumum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+
+<body>
+    
+    <nav>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    {{ $slot }} {{-- the curly bracket translate to php echo - like a shortcut --}}
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () { //declaring a route that listens to get request
+    return view('home');
+});
+
+Route::get('/about', function () { //declaring a route that listens to get request
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact'); 
 });
